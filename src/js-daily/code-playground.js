d2ef7b29501e49fe8920739f9c2faf129368e02c
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-05-29 16:34:56 +0100

    new day new challenge

--- src/js-daily/code-playground.js
+++ src/js-daily/code-playground.js
@@ -1,5 +1,21 @@
-function basicOp(operation, value1, value2) {
-  return eval(value1 + operation + value2);
+function comp(array1, array2) {
+  let result1 = [];
+  let result2 = [];
+  let allResult = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    result1.push(array1[i]);
+  }
+
+    for (let i = 0; i < array2.length; i++) {
+    result2.push(array1[i]);
+  }
+
+  if (result1 || results2) {
+    array1[i]*array2[i].} 
 }
 
-console.log(basicOp("+", 4, 7));
+let a = [121, 144, 19, 161, 19, 144, 19, 11];
+let b = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
+
+console.log(comp(a));
