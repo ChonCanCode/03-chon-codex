9af52762265f2e344ee91457326b69b6af797d8d
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-07-14 08:52:42 +0100

    adding react need know

---
+++ src/pages/React.tsx
@@ -0,0 +1,12 @@
+export default function React() {
+  return (
+    <>
+      <div>
+        <h1>File creation</h1>
+        <div>
+          <code>{`npm install react react-dom`}</code>
+        </div>
+      </div>
+    </>
+  );
+}
