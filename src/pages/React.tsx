e9b34c59aa85f059f168d233103a68bf37519c07
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-07-15 15:07:28 +0100

    adding more

--- src/pages/React.tsx
+++ src/pages/React.tsx
@@ -4,7 +4,11 @@ export default function React() {
       <div>
         <h1>File creation</h1>
         <div>
+          <span>Installing react-dom for SPA with different pages</span>
           <code>{`npm install react react-dom`}</code>
+
+          <span>Cloning Project from another device</span>
+          <code>{`git clone (github URL)`}</code>
         </div>
       </div>
     </>
